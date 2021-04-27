7b6f3880a832052cc979ba468855c666818026c2
MeHigh22 <pmihai31@gmail.com>
2021-04-27 09:25:01 +0200

    Exo1While

---
+++ script.js
@@ -0,0 +1,14 @@
+let tableau = [];
+
+let max = prompt("Choissisez un nombre max d'étudiants");
+
+while (tableau.length != max) {
+    tableau.push(prompt("rentrez un nom"))
+}
+console.log(tableau);
+
+
+console.log(classe);
+//Nous avons une variable qu'on va créer.
+//On choisit le nombre d'étudiants max dans la classe.
+
